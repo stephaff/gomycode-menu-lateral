a7bcfa56bc8accb1ad380ef712c8368e189a22bb
stephaff <affalistephane@gmail.com>
2022-07-05 21:48:25 +0000

    second

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
+liens = document.querySelectorAll('.lien');
+
+liens[0].classList.add('colorDeFond');
+
+liens.forEach(lien => {
+    lien.addEventListener('click', lienSelectionne);
+});
+
+function lienSelectionne(){
+    liens.forEach(lien => {
+        lien.classList.remove('colorDeFond');
+    });
+    this.classList.add('colorDeFond');
+}
+
+// liens.forEach(lien => {
+//     lien.addEventListener('mouseover', lienSurvole);
+// });
+
+// function lienSurvole(){
+//     liens.forEach(lien => {
+//         lien.classList.remove('colorDeFond');
+//     });
+//     this.classList.toggle('colorDeFond');
+// }
+
 let btnBack = document.querySelector('.button-back');
 
 btnBack.addEventListener('click', replier);
 
 function replier(){
-    let lienTexte = document.querySelectorAll('.lien-texte');
     document.querySelector('.menu').classList.toggle('replier');
-    for(let i=0; i<lienTexte.length; i++){
-        lienTexte[i].classList.toggle('cacher');
-    }
     this.classList.toggle('position-bouton');
 }
